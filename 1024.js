e3a373c1987ccd79b6638e881144f6f34ccb45e0
KuanningLU <411630097@o365.tku.edu.tw>
2024-10-24 11:26:44 +0800

    1024

---
+++ 1024.js
@@ -0,0 +1,8 @@
+let a=1;
+console.log(a);
+var b;
+b = 2;
+console.log(b);
+var c =123;
+console.log(c);
+
---
+++ test.ts
@@ -0,0 +1,15 @@
+const test:string = "test"
+const test2:String = "test"
+
+function testFunc(){
+    console.log("1231")
+}
+
+const testFun2 = ()=>{
+    this
+}
+
+interface human{
+    name:string
+    
+}
